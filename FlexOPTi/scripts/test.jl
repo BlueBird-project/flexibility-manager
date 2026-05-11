815c8c5e62d76ee648d4b0fafc6c927dcf410cc5
kyril <kahka@dtu.dk>
2026-05-11 10:43:05 +0200

    Change the data folder structure with inputs and outputs

--- FlexOPTi/scripts/test.jl
+++ FlexOPTi/scripts/test.jl
@@ -10,9 +10,13 @@ using Debugger
 using FlexOPTi
 
 ## Test
-dt_file = joinpath("/home/kahka/DTU/BlueBird/flexmanager/FlexOPTi/data/dynamics_estimator_results.json");
-sensors_file = joinpath("/home/kahka/DTU/BlueBird/flexmanager/FlexOPTi/data/df_predict.json");
-forecast_file = joinpath("/home/kahka/DTU/BlueBird/flexmanager/FlexOPTi/data/dynamics_estimator_results.json");
+dt_file = joinpath(@__DIR__, "../data/montcada/inputs/dynamics_estimator_results.json");
+sensors_file = joinpath(@__DIR__, "../data/montcada/inputs/df_predict.json");
+forecast_file = joinpath(@__DIR__, "../data/montcada/inputs/dynamics_estimator_results.json");
  
 ## optimize 
 oy = optimize(dt_file, sensors_file, forecast_file; loglevel="info", Hu = 2, pilot = "Montcada", solver = "Gurobi", compute_datetime = "2025-07-15T17:00:00+00:00");
+
+## Save outputs to json 
+opt_data = FlexOPTi.parse_OPT_output(oy)
+FlexOPTi.write_outputs_to_file(opt_data; file=joinpath(@__DIR__, "../data/montcada/outputs/output.json"))
